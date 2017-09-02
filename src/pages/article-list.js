a10607bf3600f4ba7760ead9111a00fad14b3605
Gogs <gogitservice@gmail.com>
2017-09-03 00:50:46 +0800

    article list page

--- src/pages/article-list.js
+++ src/pages/article-list.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import InfiniteScroll from 'react-infinite-scroller';
 import logo from '../logo.svg';
 import '../App.css';
 
 class ArticleList extends Component {
+  constructor(props) {
+      super(props);
+
+      var items = [];
+      for (var i = 0; i < 20; i++) {
+          items.push(<li><Link to="/article/detail/1">技术博客重构之路</Link></li>);
+      }
+
+      this.state = {
+          items: items,
+          hasMoreItems: true
+      };
+
+      this.loadFunc = this.loadFunc.bind(this);
+  }
+
+  loadFunc(page) {
+    var items = this.state.items;
+    var hasMoreItems = this.state.hasMoreItems;
+    if (page < 30) {
+        this.setState({
+            hasMoreItems: false
+        });
+        for (var i = 0; i < 20; i++) {
+            items.push(<li><Link to="/article/detail/1">技术博客重构之路</Link></li>);
+        }
+    } else {
+        hasMoreItems = false;
+    }
+
+    this.setState({
+        items: items,
+        hasMoreItems: hasMoreItems
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +49,16 @@ class ArticleList extends Component {
           <h2><Link to="/">罗晓俊の博客</Link></h2>
         </div>
 	<p>Building...</p>
-	<Link to="/article/detail/1">技术博客重构之路</Link>
+	<ul>
+	    <InfiniteScroll
+    	        pageStart={0}
+                loadMore={this.loadFunc}
+    	        hasMore={this.state.hasMoreItems}
+                loader={<div className="loader">Loading ...</div>}
+            >
+	        {this.state.items}
+            </InfiniteScroll>
+        </ul>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
