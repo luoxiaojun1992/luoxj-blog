fe605813b5fe62a96562cfa2a0d847ab95bbb8a5
Gogs <gogitservice@gmail.com>
2017-09-05 23:01:30 +0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Button from 'antd/lib/button';
 import BackTop from 'antd/lib/back-top';
-import logo from './logo.svg';
+import Layout, {Header, Content, Footer } from 'antd/lib/layout';
+import Breadcrumb from 'antd/lib/breadcrumb';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2><Link to="/">罗晓俊の博客</Link></h2>
-        </div>
-	<ul>
+                	<Layout>
+  		<Header style={{ position: 'fixed', width: '100%' }}>
+          		<h2><Link to="/">罗晓俊の博客</Link></h2>
+		</Header>
+    			<Content style={{ padding: '0 50px', marginTop: 64 }}>
+			<Breadcrumb style={{ margin: '12px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+      </Breadcrumb>
+			<div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
+			<ul>
 	    <li><Link to="/article/detail/1">技术博客重构之路</Link></li>
 	    <li><Link to="/article/detail/1">技术博客重构之路</Link></li>
 	    <li><Link to="/article/detail/1">技术博客重构之路</Link></li>
@@ -26,10 +31,10 @@ class App extends Component {
 	    <li><Link to="/article/detail/1">技术博客重构之路</Link></li>
 	</ul>
         <Link to="/article/list">更多></Link>
-	<Button type="primary">Button</Button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+			</div>
+			</Content>
+  		<Footer style={{ textAlign: 'center' }}>罗晓俊の博客 ©2017 Created by Roy</Footer>
+	</Layout>
 	<BackTop />
       </div>
     );
