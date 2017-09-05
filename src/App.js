1cbcfe73c3ca7690ba5eab112a1eb28157ebe3d2
罗晓俊 <luoxiaojun1992@sina.cn>
2017-09-05 19:59:13 +0800

    add layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Layout, Sider, Content, Footer } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
@@ -27,6 +28,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+	<Layout>
+	    <Header>header</Header>
+	    <Layout>
+	        <Sider>left sidebar</Sider>
+	            <Content>main content</Content>
+	        <Sider>right sidebar</Sider>
+	    </Layout>
+	    <Footer>footer</Footer>
+	</Layout>
       </div>
     );
   }
