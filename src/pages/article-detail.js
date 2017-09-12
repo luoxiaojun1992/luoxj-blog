638a69ae3bd9d642a3b202168a91fa1456f757e2
Gogs <gogitservice@gmail.com>
2017-09-12 23:04:02 +0800

    article detail

--- src/pages/article-detail.js
+++ src/pages/article-detail.js
@@ -5,17 +5,43 @@ import Layout, {Header, Content, Footer } from 'antd/lib/layout';
 import Breadcrumb from 'antd/lib/breadcrumb';
 import '../App.css';
 import ReactMarkdown from 'react-markdown';
+import config from 'react-global-configuration';
 
 class ArticleDetail extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            article : {
+                title: "",
+                content: "",
+                author: "",
+                category_name: "",
+                created_at: ""
+            }
+        };
+
+        this.fetchArticle(this);
+    }
+
+    fetchArticle(thisObj) {
+        fetch(config.get('api_gateway') + '/article/action/detail?id=' + thisObj.props.match.params.id, {
+            method: 'GET',
+            headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        }).then(function (res) {
+            if (res.ok) {
+                res.json().then(function (jsonData) {
+                    if (jsonData.code === 0) {
+                        thisObj.setState({
+                            article: jsonData.data
+                        });
+                    }
+                });
+            }
+        });
     }
 
     render() {
-        const input = '# This is a header\n\nAnd this is a paragraph';
-
         return (
             <div className="App">
                 <Layout>
@@ -28,7 +54,7 @@ class ArticleDetail extends Component {
                             <Breadcrumb.Item>文章内容</Breadcrumb.Item>
                         </Breadcrumb>
                         <div style={{background: '#fff', padding: 24, minHeight: 380}}>
-                            <ReactMarkdown source={input}/>
+                            <ReactMarkdown source={this.state.article.content}/>
                         </div>
                     </Content>
                     <Footer style={{textAlign: 'center'}}>罗晓俊の博客 ©2017 Powered by Roy</Footer>
