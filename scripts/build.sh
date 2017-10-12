d9b8c52930e59424e6958f6fd5b29d7a876bb3e7
罗晓俊 <luoxiaojun1992@sina.cn>
2017-10-12 17:47:35 +0800

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh root@114.55.142.227 -l root 'supervisorctl stop luoxj-blog && cd ~/luoxj-blog && git pull && npm install && npm run build && supervisorctl start luoxj-blog'
+ssh root@114.55.142.227 -l root 'releaseId=luoxj-blog-release-`date +%Y%m%d%H%M%s` && cp -r ~/luoxj-blog ~/$releaseId && cd ~/$releaseId && git pull && npm install && npm run build && rm -rf ~/luoxj-blog && cp -r ~/$releaseId ~/luoxj-blog && supervisorctl restart luoxj-blog'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
